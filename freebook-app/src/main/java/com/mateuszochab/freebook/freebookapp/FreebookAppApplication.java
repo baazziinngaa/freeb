b602dd2e463e7555743429b42f7c0f178b0ea793
mateusz <mateusz@DESKTOP-42GM5S5>
2018-04-28 18:48:55 +0200

    freebook first commit

---
+++ freebook-app/src/main/java/com/mateuszochab/freebook/freebookapp/FreebookAppApplication.java
@@ -0,0 +1,12 @@
+package com.mateuszochab.freebook.freebookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreebookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreebookAppApplication.class, args);
+	}
+}
