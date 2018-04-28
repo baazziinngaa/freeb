52b5d104c24e7b910c015cf85b496fd58ac8b13e
mateusz <mateusz@DESKTOP-42GM5S5>
2018-04-28 18:53:35 +0200

    add freebook cloud server

---
+++ freebook-cloud-config-server/src/test/java/com/mateuszochab/freebook/freebookcloudconfigserver/FreebookCloudConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mateuszochab.freebook.freebookcloudconfigserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FreebookCloudConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
